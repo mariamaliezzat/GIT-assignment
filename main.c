e49111b93812a11ae812a7063e2a5f5873f2fe48
mariamaliezzat <mariamali.ezzat@gmail.com>
2021-04-12 17:24:16 +0200

    main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct students{
+	char name[50];
+	unsigned int ID;
+	unsigned int birth_day;
+	unsigned int birth_month;
+	unsigned int birth_year;
+	signed int score;
+	
+};
+
+
+int main()
+{
+    printf("*************************************************\n  welcome to our repository");
+
+    return 0;
+}
