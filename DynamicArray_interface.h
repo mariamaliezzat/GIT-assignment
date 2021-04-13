d0776898f308cf0ef783ae3dbe1c3992c660ce44
Alyaa <alyaa.moh.bakr@gmail.com>
2021-04-13 13:21:12 +0200

    DynamicArray branch

---
+++ DynamicArray_interface.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+ *
+ * File Name:   DynamicArray_interface.h
+ *
+ * Description: functions declaration used for the dynamic array
+ *
+ * Date:        4/13/2021
+ *
+ ******************************************************************************/
+ 
+#ifndef DYNAMICARRAY_INTERFACE_H_
+#define DYNAMICARRAY_INTERFACE_H_
+ 
+ void void_DynArr_insertBeginning(void);
+ void void_DynArr_insertMiddle(void);
+ void void_DynArr_insertEnd(void);
+ 
+ void void_DynArr_printArr(void);
+ 
+ 
+#endif 
