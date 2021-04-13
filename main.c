c52b7c3cdf3357e228df7b96a31a83d6096fceef
mariamaliezzat <71596389+mariamaliezzat@users.noreply.github.com>
2021-04-13 14:43:31 +0200

    Update main.c
    adding function to calculate execution time in milliseconds

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "struct.h"
-
+double END_Timer(clock_t begin)
+{
+	clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	return time_spent*1000;
+}
 int main()
 {
     printf("*************************************************\n  welcome to our repository");
